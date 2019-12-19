d1a49ea4f8f30fa8c3d1b29117cbe7dbff13ea5e
nikJ <jmi6034@gmail>
2019-12-19 10:48:05 +0900

    뉴스 페이지 추가

---
+++ pages/news/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <VueSlickCarousel :arrows="true" :dots="true">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import VueSlickCarousel from 'vue-slick-carousel'
+
+export default {
+  name: 'MyComponent',
+  components: { VueSlickCarousel }
+}
+</script>
