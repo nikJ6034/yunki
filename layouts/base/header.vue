51dc76be3b9e02cd65c66abe60cff2a3230a4a7c
nikJ <jmi6034@gmail.com>
2020-01-07 15:49:24 +0900

    commit

--- layouts/base/header.vue
+++ layouts/base/header.vue
@@ -1,21 +1,72 @@
 <template>
   <div>
     <b-container>
-      <b-row align-v="center" style="height: 100px;" >
-        <b-col cols="2">
-          <b-img left src="~/static/images/정의당.png" fluid alt="Responsive image" style="height:100px"></b-img>
-        </b-col>
-        <b-col cols="6">
-                    <h3>대전 유성구을 <br/>국회의원 예비후보 김윤기</h3>
-        </b-col>
-        <b-col  cols="4" class="m-0 p-0">
-          <b-nav >
-            <b-nav-item active style="color:#ffcb0a;">홈</b-nav-item>
-            <b-nav-item >공약</b-nav-item>
-            <b-nav-item >활동사진</b-nav-item>
-          </b-nav>
+      <b-row>
+        <b-col cols="12">
+          <b-row class="notMobile h-100 m-0">
+            <div style="display:inline-block; width: 20%;">
+              <b-img class="logo" src="~/static/images/정의당_높이조절.png" alt="Responsive image"></b-img>
+            </div>
+            <div style="display:inline-block; width: 80%;">
+              <div class="title h-100" style="display:inline-block ">
+                <!-- <div class="h-100" style="display:inline-block; font-size:15px; vertical-align: middle"></div> -->
+                <div class="title1" style="display:inline-block;">대전 유성구을 국회의원</div><br/>
+                <div class="title2" style="display:inline-block;">예비후보</div>
+                <div class="title3" style="display:inline-block;">김윤기</div>
+              </div>
+            </div>
+            <!-- <div style="margin-right:-20px; display:inline-block">
+              <b-nav class="text-muted h-100">
+                <div class="h-100" style="display:inline-block; font-size:15px; vertical-align: middle"></div>
+                <b-nav-item class="text-muted" href="#">홈</b-nav-item>
+                <b-nav-item class="text-muted" href="#">유튜브</b-nav-item>
+                <b-nav-item class="text-muted" href="#">공약</b-nav-item>
+              </b-nav>
+            </div> -->
+          </b-row>
+          <div class="mobile">
+            <b-navbar toggleable="lg">
+              <div style="display:inline-block; width:100%">
+                <b-img class="logo w-px-100" left src="~/static/images/정의당_높이조절.png" alt="Responsive image"></b-img>
+                <div class="title w-px-200" style="">
+                  <div style="display:inline-block; font-size:12px;">대전 유성구을 국회의원</div><br/>
+                  <div class="mr-2" style="display:inline-block; font-size:10px;">예비후보</div><div style="display:inline-block; font-size:25px;">김윤기</div>
+                </div>
+              </div>
+              <!-- <b-navbar-brand href="#">홈</b-navbar-brand> -->
+              <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+              <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                  <b-nav-item class="text-muted" href="#">홈</b-nav-item>
+                  <b-nav-item class="text-muted" href="#">유튜브</b-nav-item>
+                  <b-nav-item class="text-muted" href="#">공약</b-nav-item>
+                </b-navbar-nav>
+              </b-collapse> -->
+            </b-navbar>
+          </div>
         </b-col>
       </b-row>
     </b-container>
   </div>
 </template>
+
+<style scoped>
+.logo {display:inline-block;}
+.h-px-100 {height:100px;}
+.w-px-100 {width:100px;}
+
+@media (max-width: 960px) {
+  .notMobile {display:none}
+  div.title {font-weight: bold; text-align: end;}
+}
+/*피씨 버전*/
+@media (min-width: 960px) {
+  .mobile {display:none}
+  div.title {padding:10px;text-align: end; width:100%;}
+  div.title .title1 {font-size: 20px; font-weight: bold;}
+  div.title .title3 {font-size: 30px; font-weight: bold;}
+  div.title .title2 {font-size: 15px}
+  ul {display:inline-block;}
+  ul li {display:inline-block;}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,26 @@
 <template>
   <div>
-    <div style="margin-top:10px">
+    <div class="mb-2">
       <div class="container">
-        <b-carousel
-          id="carousel-1"
-          v-model="slide"
-          :interval="4000"
-          controls
-          indicators
-          background="#ababab"
-          img-width="1024"
-          img-height="380"
-          style="text-shadow: 1px 1px 2px #333;"
-          @sliding-start="onSlideStart"
-          @sliding-end="onSlideEnd">
-          <!-- Slides with img slot -->
-          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-          <b-carousel-slide>
-            <template v-slot:img>
-              <img
-                class="d-block img-fluid w-100"
-                width="1024"
-                height="380"
-                src="https://picsum.photos/1024/380/?image=55"
-                alt="image slot"
-              >
-            </template>
-              <h1>Hello world!</h1>
-          </b-carousel-slide>
-        </b-carousel>
+        <!-- <VueSlickCarousel :arrows="true" :dots="true"> -->
+          <div class="carousel1">
+            <div class="title">판을갈자!</div>
+            <div class="sub1">내 삶을 위해!</div>
+            <div class="sub2">노동을 위해!</div>
+            <img src="~/static/images/크리스마스.jpg" style="width:100%">
+          </div>
+          <!-- <div class="carousel2"><img src="~/static/images/크리스마스.jpg"></div> -->
+        <!-- </VueSlickCarousel> -->
       </div>
     </div>
-    <div class="container" style="margin-top: 10px">
+    <div class="container">
       <b-row  class="m-0 ">
-        <b-col cols=12>
+        <b-col cols=12 class="mb-2">
           <div>
-            <b-img left src="~/static/images/YouTube-logo.png" fluid alt="Responsive image" style="height:60px"></b-img>
+            <b-img class="youtubeLogo" left src="~/static/images/YouTube-logo.png" fluid alt="Responsive image"></b-img>
           </div>
-          <div style="float: right;line-height: 60px;">
-            더보기
+          <div class="youtubeMore" style="float: right;">
+            <a class="text-muted" style="font-size: 18px; font-weight: 500;" href="https://www.youtube.com/channel/UC75n1YQodpOupIzTes2njMA" target="_blank">더보기</a>
           </div>
         </b-col>
         <!-- <b-col cols=2 style="text-align:end">
@@ -46,45 +28,55 @@
         </b-col> -->
       </b-row>
       <b-row >
-        <b-col cols-3>
+        <b-col cols-3 sm="12" md="4" class="mb-2">
           <b-embed
             type="iframe"
             aspect="16by9"
-            src="https://www.youtube.com/embed/MctKqr2g68Q"
+            src="https://www.youtube.com/embed/EthQVg1ayfo"
             allowfullscreen
           ></b-embed>
         </b-col>
-        <b-col cols-3>
+        <b-col cols-3 sm="12" md="4" class="mb-2">
           <b-embed
             type="iframe"
             aspect="16by9"
-            src="https://www.youtube.com/embed/tzM3l6n7ihM"
+            src="https://www.youtube.com/embed/5t_BvoIXmQM"
             allowfullscreen
           ></b-embed>
         </b-col>
-        <b-col cols-3>
+        <b-col cols-3 sm="12" md="4" class="mb-2">
           <b-embed
             type="iframe"
             aspect="16by9"
-            src="https://www.youtube.com/embed/2bh9zvKBjGA"
+            src="https://www.youtube.com/embed/5pDYBBM4whc"
             allowfullscreen
           ></b-embed>
         </b-col>
       </b-row>
     </div>
-    <div class="container" style="margin-top: 10px">
-      <b-row style="height:300px">
-        <b-col cols="5" style="border:#000000 solid 1px">
-          페이스북 게시물이 들어갈 자리
+    <div class="container">
+      <b-row>
+        <b-col sm="12" md="6" class="p-0 mb-2">
+          <div style="overflow:hidden; margin: 0 0 0 10px;">
+            <a href="https://www.facebook.com/Kimyunki" target="_blank">
+              <b-img left src="~/static/images/f_logo-Blue_100.png" fluid alt="facebook image" width="30px"></b-img>
+              <div style="font-weight: 700; color:#0058a3;margin-left: 10px;font-size: 23px;display:inline-block;float:left">김윤기</div>
+              <b-img left src="~/static/images/facebook-logo.png" fluid alt="facebook image" width="150px"></b-img>
+            </a>
+          </div>
+          <div style="height:100px;border: solid 2px #7ac4fa; border-radius: 8px; margin: 5px;" v-for="post in data.data">
+            <div style="font-weight: 500;height:110%; display:inline-block; width:15%; font-size:9px; vertical-align: middle;">{{dateFormet(post.created_time)}}</div>
+            <div class="faceContent" style="height:100%; overflow:scroll;overflow-x: hidden; display:inline-block; width:80%; padding: 10px;">{{post.message}}</div>
+          </div>
         </b-col>
-        <b-col cols="7" style="border:#000000 solid 1px">
+        <b-col sm="12" md="6" class="mb-2" style="border:#000000 solid 1px">
           공약이 들어갈 자리
         </b-col>
       </b-row>
     </div>
     <div class="container" style="margin-top: 10px">
       <b-row style="height:300px">
-        <b-col cols="12" style="border:#000000 solid 1px">
+        <b-col cols="12"  style="border:#000000 solid 1px">
           활동 사진이 들어갈 자리
         </b-col>
       </b-row>
@@ -93,15 +85,104 @@
 </template>
 
 <script>
+// import 'slick-carousel/slick/slick.css'
+// import 'slick-carousel/slick/slick-theme.css'
+// import VueSlickCarousel from 'vue-slick-carousel'
+import axios from 'axios'
+import moment from 'moment'
 
 export default {
   components: {
-
   },
-  layout: 'main'
+  layout: 'main',
+  asyncData ({ params }) {
+    return axios.get(`http://211.230.82.118:8080/facebookPosts`)
+      .then((res) => {
+        // console.log(res)
+        return { data: res.data }
+      })
+  },
+  methods: {
+    dateFormet (date) {
+      return moment(String(date)).format('YYYY년MM월DD일\n hh시mm분')
+    }
+  }
 }
 </script>
 
 <style>
+.youtubeLogo {height:60px}
+.youtubeMore {line-height: 60px;}
 
+{scrollbar-face-color: #e0e0e0;
+scrollbar-track-color: #fff;
+scrollbar-arrow-color: none;
+scrollbar-highlight-color: #e0e0e0;
+scrollbar-3dlight-color: none;
+scrollbar-shadow-color: #e0e0e0;
+scrollbar-darkshadow-color: none;}
+
+.faceContent::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+  border: 3px solid #eee;
+}
+.faceContent::-webkit-scrollbar-button:start:decrement,
+::-webkit-scrollbar-button:end:increment {
+  display: block;
+  height: 5px;
+  background: #eee;
+}
+.faceContent::-webkit-scrollbar-track {
+  background: #eee;
+  -webkit-border-radius: 10px;
+  border-radius:10px;
+}
+.faceContent::-webkit-scrollbar-thumb {
+  height: 50px;
+  width: 50px;
+  background: rgba(0,0,0,.2);
+  -webkit-border-radius: 8px;
+  border-radius: 8px;
+}
+
+[class^=carousel] {position: relative}
+.carousel1 .title{position: absolute; top: 70px; left: 50px; font-size: 100px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both; margin-left: 50%; color:#868686}
+.carousel1 .sub1{position: absolute; top: 250px; left: 100px; font-size: 50px; animation: slide-left 2s;animation-delay: 1.2s; animation-fill-mode: both; margin-left: 50%; color:#868686}
+.carousel1 .sub2{position: absolute; top: 350px; left: 150px; font-size: 50px; animation: slide-left 2s;animation-delay: 1.5s; animation-fill-mode: both; margin-left: 50%; color:#868686}
+@keyframes slide-left {
+  from {
+    margin-left: 50%;
+  }
+  to {
+    margin-left: 0%;
+  }
+}
+
+@media (max-width: 1200px) {
+  .youtubeLogo {height:30px;}
+  .youtubeMore {line-height: 30px;}
+  .carousel1 .title{position: absolute; top: 50px; left: 50px; font-size: 70px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub1{position: absolute; top: 200px; left: 100px; font-size: 30px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub2{position: absolute; top: 280px; left: 150px; font-size: 30px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+}
+@media (max-width: 960px) {
+  .youtubeLogo {height:30px;}
+  .youtubeMore {line-height: 30px;}
+  .carousel1 .title{position: absolute; top: 50px; left: 50px; font-size: 50px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub1{position: absolute; top: 150px; left: 100px; font-size: 20px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub2{position: absolute; top: 200px; left: 150px; font-size: 20px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+}
+
+@media (max-width: 768PX) {
+  .carousel1 .title{position: absolute; top: 30px; left: 50px; font-size: 30px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub1{position: absolute; top: 110px; left: 100px; font-size: 15px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub2{position: absolute; top: 150px; left: 150px; font-size: 15px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+}
+
+@media (max-width: 576px) {
+  .carousel1 .title{position: absolute; top: 30px; left: 50px; font-size: 30px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub1{position: absolute; top: 100px; left: 100px; font-size: 15px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+  .carousel1 .sub2{position: absolute; top: 140px; left: 150px; font-size: 15px; animation: slide-left 2s;animation-delay: 1s; animation-fill-mode: both;}
+}
 </style>
